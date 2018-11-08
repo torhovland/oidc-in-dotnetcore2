f58ea8c4ac218024712805518bb814b5258563b9
Tor Hovland <tor.hovland@bekk.no>
2018-11-08 12:21:36 +0100

    Copying dotnet-server-side to dotnet-server-side-plus-api.

---
+++ dotnet-server-side-plus-api/WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var user = HttpContext.User;
+            var claims = user.Claims.ToList();
+
+            return new[]
+            {
+                $"Namn: {user.Identity.Name}",
+                $"Brukarnamn (subject ID): {claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Sub)?.Value}",
+                $"Epostadresse: {claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Email)?.Value}",
+                $"Favorittfarge: {claims.FirstOrDefault(c => c.Type == "favorittfarge")?.Value}"
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ dotnet-server-side-plus-api/WebApplication/Startup.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Remove the default non-OIDC claims, otherwise the OIDC claims will not be added properly.
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie(options =>
+                {
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                    options.Cookie.Name = "auth";
+                })
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    const string favorittfarge = "favorittfarge";
+
+                    options.Authority = Configuration["Oidc:Authority"];
+                    options.ClientId = Configuration["Oidc:ClientId"];
+                    options.ClientSecret = Configuration["Oidc:ClientSecret"];
+
+                    options.ResponseType = "code id_token";
+
+                    options.Scope.Clear();
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    options.Scope.Add("email");
+                    options.Scope.Add(favorittfarge);
+
+                    options.ClaimActions.Remove("amr");
+                    options.ClaimActions.MapJsonKey(favorittfarge, favorittfarge);
+
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.SaveTokens = true;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role",
+                    };
+                });
+
+            services
+                .AddMvc()
+                .AddRazorPagesOptions(options =>
+                {
+                    options.Conventions.AuthorizePage("/about");
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
